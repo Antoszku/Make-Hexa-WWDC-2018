d93a7cdd194e89f2889f8f35cbc386325db91d4c
Dominik Rygiel <Dominik.Rygiel@silvair.com>
2018-04-03 09:16:32 +0200

    final commit

--- WWDC 2018 Dominik Rygiel.playground/Contents.swift
+++ WWDC 2018 Dominik Rygiel.playground/Contents.swift
@@ -1,5 +1,13 @@
+/*: Image Formation
+ # Make Hexa!
+ *  Drag the same-colored piece to make a hexagon
+ *  Tap to rotate the piece
+ *  For every filled hexagon you get 60 points
+ *  For every piece placed on board you get 1 point
+ */
+
 import PlaygroundSupport
+
 
 let controler = GameViewController()
 PlaygroundPage.current.liveView = controler
-
